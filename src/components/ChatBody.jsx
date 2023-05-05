c85c38b86f08362ceb261ebe1d473e7281c57402
Leonardo Gama <leogama09@gmail.com>
2023-05-05 11:00:17 -0300

    initial

---
+++ src/components/ChatBody.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useRef } from "react"
+import autoAnimate from "@formkit/auto-animate"
+
+const ChatBody = ({chat}) => {
+    const aiStyle = "bg-white bg-opacity-40 backdrop-blur-1g dropshadow-md mr-auto"
+
+    const parent = useRef(null)
+    const bottomRef = useRef(null)
+
+    // only for auto animations
+    useEffect(() => {
+        parent.current && autoAnimate(parent.current)
+    })
+
+    // for scrolling bottom
+    useEffect(() => {
+        bottomRef.current?.scrollIntoView({behavior: "smooth"})
+    }, [chat])
+
+  return (
+    <div className="flex flex-col gap-4" ref={parent}>
+        {chat.map((message, i) => {
+            return (
+                <div 
+                    key={i} 
+                    className={`border-[#999999] break-words border-2 rounded-xl self-end px-3 py-3 max-w-[80%] ${message.sender === "ai" && aiStyle}`}>
+                    <pre className='whitespace-pre-wrap'>
+                        <span>{message.message}</span>
+                    </pre>
+                </div>
+            )
+        })}
+
+        <div ref={bottomRef} className="h-3"></div>
+    </div>
+  )
+}
+
+export default ChatBody
